fbc0bc6fd9a33b97fe753097931e251a8b5ef34f
southmw <southmw@gmail.com>
2022-11-07 16:48:54 +0900

    add variables.tf default value

--- variables.tf
+++ variables.tf
@@ -3,19 +3,23 @@
 variable "client_id" {
   description = "Client ID (APP ID) of the application"
   type        = string
+  default     = ""
 }
 
 variable "client_secret" {
   description = "Client Secret (Password) of the application"
   type        = string
+  default     = ""
 }
 
 variable "subscription_id" {
   description = "Subscription ID"
   type        = string
+  default     = ""
 }
 
 variable "tenant_id" {
   description = "Tenant ID"
   type        = string
+  default     = ""
 }
